a71a3a24ed24e60cf2a421edc4da9d467982f579
Douglas Kuhn <douglas@composable.finance>
2023-03-21 19:46:42 -0400

    Create kstroke.rs
    Keystroke capture in Rust

---
+++ kstroke.rs
@@ -0,0 +1,27 @@
+use std::io::{self, Read};
+use termion::event::{Event, Key};
+use termion::input::TermRead;
+
+fn main() -> io::Result<()> {
+    // Set terminal to raw mode to read key events
+    let stdin = io::stdin();
+    let mut stdout = io::stdout().into_raw_mode()?;
+
+    // Enable mouse mode to allow clicking events
+    write!(stdout, "{}", termion::cursor::Hide)?;
+    write!(stdout, "{}", termion::terminal::EnterMouseMode)?;
+
+    // Listen for key events and print them to console
+    for event in stdin.events() {
+        match event.unwrap() {
+            Event::Key(Key::Char(c)) => println!("Key pressed: {}", c),
+            Event::Key(Key::Esc) => break,
+            _ => {}
+        }
+    }
+
+    // Restore terminal state
+    write!(stdout, "{}", termion::cursor::Show)?;
+    write!(stdout, "{}", termion::terminal::LeaveMouseMode)?;
+    Ok(())
+}
