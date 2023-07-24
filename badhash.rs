0e7ae680ee5a26ee69080f312abbe9ea5c4955d3
Douglas Kuhn <douglas@composable.finance>
2023-07-24 18:20:02 -0400

    Update badhash.rs
    Updated badhash.

--- badhash.rs
+++ badhash.rs
@@ -1,26 +1,32 @@
+// Import only what's needed
+use std::hash::{BuildHasherDefault, Hasher, BuildHasher};
 use std::collections::HashMap;
-use std::hash::{Hasher, BuildHasherDefault};
-use std::collections::hash_map::RandomState;
 
+// Implement the Hasher trait for BadHash
 struct BadHash;
 
-impl std::hash::Hasher for BadHash {
-    fn write(&mut self, _bytes: &[u8]) {
-        // Your implementation here
+impl Hasher for BadHash {
+    fn finish(&self) -> u64 {
+        0 // Return a constant for simplicity
     }
 
-    fn finish(&self) -> u64 {
-        // Your implementation here
-        42 // Dummy value
+    fn write(&mut self, _bytes: &[u8]) {
+        // Do nothing for simplicity
     }
 }
 
-type BadBuildHasher = BuildHasherDefault<BadHash>;
+// Implement the BuildHasher trait for BadHash
+impl BuildHasher for BuildHasherDefault<BadHash> {
+    type Hasher = BadHash;
 
+    fn build_hasher(&self) -> Self::Hasher {
+        BadHash
+    }
+}
+
+// Now you can use BadHash as the hasher in your HashMap
 fn main() {
-    let bad_hash = BadBuildHasher::default();
-    let mut map = HashMap::with_hasher(bad_hash);
-
+    let mut map = HashMap::default(BuildHasherDefault::<BadHash>::default());
     map.insert("key1", "value1");
     map.insert("key2", "value2");
     map.insert("key3", "value3");
