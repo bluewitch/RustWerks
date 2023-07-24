967025a8e5ebeb6a1fd4ac826554a3d39e79de3b
Douglas Kuhn <douglas@composable.finance>
2023-07-23 22:41:03 -0400

    Update badhash.rs
    New Update

--- badhash.rs
+++ badhash.rs
@@ -1,6 +1,25 @@
+use std::collections::HashMap;
+use std::hash::{Hasher, BuildHasherDefault};
+use std::collections::hash_map::RandomState;
+
+struct BadHash;
+
+impl std::hash::Hasher for BadHash {
+    fn write(&mut self, _bytes: &[u8]) {
+        // Your implementation here
+    }
+
+    fn finish(&self) -> u64 {
+        // Your implementation here
+        42 // Dummy value
+    }
+}
+
+type BadBuildHasher = BuildHasherDefault<BadHash>;
+
 fn main() {
-    let bad_hash = BadHash;
-    let mut map = std::collections::HashMap::with_hasher(bad_hash);
+    let bad_hash = BadBuildHasher::default();
+    let mut map = HashMap::with_hasher(bad_hash);
 
     map.insert("key1", "value1");
     map.insert("key2", "value2");
